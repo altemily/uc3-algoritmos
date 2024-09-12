19a0ead9b27a3698adfe0ed62da9929c4c608462
Ariane Carvalho <arianecarvalho@MacBook-Air-de-Ariane.local>
2024-09-12 00:15:01 -0300

    add: Adicionando lista while e for ao repositório.

---
+++ lista-while-for/lista.js
@@ -0,0 +1,93 @@
+// // QUESTÃO 1
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+ 
+
+// // QUESTÃO 2
+
+// let i = 0;
+// while (i<=10){
+//     console.log(i);
+// i++
+// }
+
+// QUESTÃO 3
+
+// let s = 0
+// for (let i =1; i <= 5; i++){
+//     s +=i;
+// }
+// console.log (s)
+
+// QUESTÃO 4
+
+// let s = 0;
+// let i = 1;
+
+// while (i<=5){
+// s +=i;
+// i++
+// }
+
+// console.log(s)
+
+
+// QUESTÃO 5
+
+// let s = 1
+// for (let i =1; i <=4 ; i++){
+//     s *=i;
+// }
+// console.log (s)
+
+
+// QUESTÃO 6
+
+
+// let s = 1;
+// let i = 1;
+
+// while (i<=4){
+// s *=i;
+// i++
+// }
+
+// console.log (s)
+
+
+// QUESTÃO 7
+
+
+//  for (let i=1; 1<=10; i++){
+//      if (i % 2 == 0){
+//         console.log(i, 'Par')
+//      }}
+// for (let i=1; 1<=10; i++){
+//     if (i % 2 !==0){
+//         console.log(i, 'Ímpar')
+//     }
+// }
+
+
+// QUESTÃO 8
+
+
+// let i = 1
+// while (i<=10){
+//     if (i % 2 == 0){
+//         console.log (i, 'Par')
+//     }
+// }
+
+// i++
+
+// let x = 1
+// while (x<=10){
+//     if (x % 2 !== 0){
+//         console.log (x, 'Ímpar')
+//     }
+// }
+
+// x++
