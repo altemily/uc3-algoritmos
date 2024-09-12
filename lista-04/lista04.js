394c25c2a4b89ed0410e7b5788c950a7ec6abf17
Ariane Carvalho <arianecarvalho@MacBook-Air-de-Ariane.local>
2024-09-11 23:52:51 -0300

    add: Adicionando a lista 04 ao repositório.

---
+++ lista-04/lista04.js
@@ -0,0 +1,169 @@
+
+  
+//   // QUESTÃO 1
+
+//   // function classificarNota(){
+//   // const nota = parseInt(prompt('Digite a nota do aluno (0 a 100):'));
+//   // let resultado;
+//   // if (nota > 90){
+//   // resultado = 'A. Excelente trabalho!';
+//   // } else if (nota >= 80){
+//   // resultado = 'B. Bom trabalho!';
+//   // } else if (nota >= 70){
+//   // resultado = 'C. Trabalho satisfatório.';
+//   // } else if (nota >= 60){
+//   // resultado = 'D. Precisa melhorar.';
+//   // } else{
+//   // resultado = 'F. Insuficiente.';
+//   // }
+//   // alert(`Sua nota é ${resultado}`);
+//   // }
+//   // classificarNota();
+  
+//   // QUESTÃO 2
+  
+//   // function classificarLimite(grade, padrao) {
+//   // let limite = parseInt(prompt(`Digite o limite superior para a nota ${grade} (número
+//   inteiro):`), 10);
+//   // if (limite > 0) {
+//   // return limite;
+//   // } else {
+//   // return padrao;
+//   // }
+//   // }
+//   // function classificarNota(nota, limiteA, limiteB, limiteC) {
+//   // if (nota >= limiteA) {
+//   // return 'A';
+//   // } else if (nota >= limiteB) {
+//   // return 'B';
+//   // } else if (nota >= limiteC) {
+//   // return 'C';
+//   // } else {
+//   // return 'D';
+//   // }
+//   // }
+//   // let limiteA = classificarLimite('A', 90);
+//   // let limiteB = classificarLimite('B', 80);
+//   // let limiteC = classificarLimite('C', 70);
+//   // let nota = parseInt(prompt("Digite a nota que deseja classificar (número inteiro):"), 10);
+//   // let classificacao = classificarNota(nota, limiteA, limiteB, limiteC);
+//   // alert(`A nota ${nota} foi classificada como: ${classificacao}`);
+  
+//   // QUESTÃO 3
+  
+//   // function classificarNota(){
+//   // const nota = parseInt(prompt('Digite a nota do aluno (0 a 100):'));
+//   // // let resultado;
+//   // if (nota = 0 && nota <= 100){
+//   // resultado = "Sua nota é válida.";
+//   // }
+//   // else if (nota < 0 && nota > 100 ){
+//   // resultado = ("Sua nota é inválida. Por favor, verifique e digite novamente.");
+//   // }
+  
+//   // alert(`Sua nota é ${resultado}`);
+//   // }
+  
+//   // classificarNota();
+  
+//   // QUESTÃO 4
+  
+//   // function classificarNota(){
+//   // const nota = parseInt(prompt('Digite a nota do aluno (0 a 100):'));
+//   // let resultado;
+//   // if (nota <= 60 && nota <= 100){
+//   // resultado = "Você está aprovado.";
+//   // }
+//   // else if (nota <= 55 && nota < 60){
+//   // resultado = ("Você está quase lá!");
+//   // }
+//   // else {
+//   // resultado = ("Nota insuficiente");
+//   // }
+//   // alert(`${resultado}`);
+//   // }
+  
+//   // classificarNota();
+  
+//   // QUESTÃO 5
+//   // function classificarNota(){
+//   // const nota = parseInt(prompt('Digite a nota do aluno (0 a 100):'));
+//   // let sugestao;
+//   // if (nota > 90){
+//   // sugestao = 'A. Continue assim!';
+//   // } else if (nota >= 80){
+//   // sugestao = 'B. Bom trabalho!';
+//   // } else if (nota >= 70){
+  
+//   // sugestao = 'C. Vamos estudar mais?!.';
+//   // } else if (nota >= 60){
+//   // sugestao = 'D. Foque nos estudos, você consegue.';
+//   // } else{
+//   // sugestao = 'F. Alerta!! Alerta!! Se liga, cabeção!! Precisa melhorar URGENTE.';
+//   // }
+//   // alert(`Sua nota é ${sugestao}`);
+//   // }
+//   // classificarNota();
+  
+//   // QUESTÃO 6
+  
+//   // function classificarNota(){
+//   // const nota = parseInt(prompt('Digite a nota do aluno (0 a 100):'));
+//   // const aluno = parseInt(prompt('Insira seu comentário.'));
+//   // let sugestao;
+//   // if (nota > 90){
+//   // sugestao = 'A. Continue assim!';
+//   // } else if (nota >= 80){
+//   // sugestao = 'B. Bom trabalho!';
+//   // } else if (nota >= 70){
+//   // sugestao = 'C. Vamos estudar mais?!.';
+//   // } else if (nota >= 60){
+//   // sugestao = 'D. Foque nos estudos, você consegue.';
+//   // } else{
+//   // sugestao = 'F. Alerta!! Alerta!! Se liga, cabeção!! Precisa melhorar URGENTE.';
+//   // }
+//   // alert(`Sua nota é ${sugestao}`);
+//   // }
+//   // classificarNota();
+  
+//   // QUESTÃO 7
+//   // function classificarNota(){
+//   // const nota = parseInt(prompt('Digite a nota do aluno (0 a 100):'));
+//   // let faixa;
+//   // if (nota > 90){
+//   // faixa = 'A.';
+//   // } else if (nota >= 80 && nota <= 71){
+//   // faixa = 'B.';
+//   // } else if (nota >= 70 && nota >= 61){
+//   // faixa = 'C.';
+//   // } else if (nota >= 60){
+  
+//   // faixa = 'D.';
+//   // } else{
+//   // faixa = 'F.';
+//   // alert(`Sua nota está na faixa ${faixa}`);
+//   // }
+//   // }
+//   // classificarNota();
+  
+//   // // QUESTÃO 8
+//   // function classificarNota(){
+//   // const nota = parseInt(prompt('Digite a nota do aluno (0 a 100):'));
+//   // let notaMaxima;
+//   // let notaMinima;
+//   // if (notaMinima > 91 && notaMaxima <= 100) {
+//   // alert(`A. A faixa de nota é de 91 a 100.`);
+//   // }
+//   // else if (notaMinima > 81 && notaMaxima <=90){
+//   // alert(`B. A faixa de nota é de 81 a 90.`);
+//   // }
+//   // else if (notaMinima > 71 && notaMaxima <= 80)
+//   // alert(`C. A faixa de nota é de 71 a 80.`); {
+//   // }
+//   // else if (notaMinima > 61 && notaMaxima <= 70)
+//   // alert(`D. A faixa de nota é de 61 a 70.`); {
+//   // }
+//   // else
+//   // alert(`F. A faixa de nota é de 0 a 60.`);
+//   // }
+//   // classificarNota();
