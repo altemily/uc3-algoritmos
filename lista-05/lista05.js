68fd8ab4e9411f697d90290243b7dd7546a7b94b
Ariane Carvalho <arianecarvalho@MacBook-Air-de-Ariane.local>
2024-09-12 00:06:31 -0300

    add: Adicionando a lista 05 ao repositório.

---
+++ lista-05/lista05.js
@@ -0,0 +1,73 @@
+// QUESTÃO 1
+
+// let frutas = ['maça','banana','laranja']
+// frutas.unshift('abacaxi')
+// console.log(frutas)
+// frutas.push('manga')
+// console.log(frutas)
+// frutas.shift()
+// console.log(frutas)
+// frutas.pop()
+// console.log(frutas)
+
+// QUESTÃO 2
+
+// let numeros = [10,20,30,40,50]
+// numeros.splice(2,0,25)
+// console.log(numeros)
+
+
+// QUESTÃO 3
+
+// let cores = ['vemelho','azul', 'verde','amarelo','azul']
+// console.log(cores.indexOf('azul'))
+// console.log(cores.lastIndexOf('azul'))
+// console.log(cores.includes('roxo'))
+
+
+// QUESTÃO 4
+
+// let alunos = ['João','Ana','Carlos','Bia']
+// const meio = alunos.splice(alunos.indexOf('Carlos'),1)
+// console.log(meio)
+// console.log(alunos)
+// alunos.push('Maria')
+// console.log(alunos)
+// alunos.unshift('Pedro')
+// console.log(alunos)
+
+
+// QUESTÃO 5
+
+// let animais = ['gato','cachorro','pássaro','peixe','gato']
+// animais.shift('gato')
+// console.log(animais)
+// animais.splice(animais.indexOf('pássaro'),0)
+// console.log(animais)
+// console.log(animais.includes('gato'))
+
+
+// QUESTÃO 6 **Corrigir**
+
+// let livros = ['O Senhor dos Anéis', '1994', 'O Pequeno Príncipe', 'Harry Potter']
+// console.log(livros.indexOf('1994'))
+// livros.splice(livros.indexOf('O Pequeno Príncipe'),0)
+// console.log(livros)
+
+
+
+
+
+// QUESTÃO 7
+
+// let cidades = ['São Paulo','Rio de Janeiro', 'Salvador','Brasília']
+// console.log(cidades.includes('Salvador'))
+// cidades.unshift('Curitiba')
+// console.log(cidades)
+
+
+// QUESTÃO 8
+
+let jogos = ['FIFA','Minecraft','Fortnite','Among Us']
+jogos.shift('FIFA')
+console.log
