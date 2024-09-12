bd655b65f632e310849d6fe513be195abb1eef01
Ariane Carvalho <arianecarvalho@MacBook-Air-de-Ariane.local>
2024-09-11 23:33:32 -0300

    add: Adicionando a lista 01 ao repositório.

---
+++ lista-01/lista01.js
@@ -0,0 +1,93 @@
+// LISTA 01
+
+
+// QUESTÃO 1
+
+let valor = 120
+
+if(valor>100){
+     console.log('Você tem um desconto de 20%!')
+ }
+else if(valor<50){
+   console.log('Você tem um desconto de 10%')
+ }
+
+
+//  Questão 2
+
+
+ let idade = 20
+
+if(idade<12){
+    console.log('Criança')
+ }
+else if(idade<12&&idade>18){
+    console.log('Adolecente')
+ }
+else{
+     console.log('Adulto')
+ }
+
+//  Questão 3
+
+let temperatura = 2
+
+ if(temperatura<15){
+    console.log('Frio')
+ }
+ else if(temperatura>15&&temperatura<30){
+    console.log('Agradável')
+ }   
+else {
+    console.log('Quente')
+ }
+
+
+
+
+//  Questão 4
+
+let nota = 60
+
+ if(nota>90){
+     console.log('A')
+ }
+
+else if (nota>70&&nota<90){
+     console.log('B')
+ }
+else{
+     console.log('C')
+ }
+
+
+
+//  Questão 5
+
+let velocidade = 120
+
+if(velocidade>120){
+     console.log('Muito rápido')
+ }
+ else if (velocidade>80&&velocidade<120){
+     console.log('Velocidade adequada')
+}
+ else{
+    console.log('Muito lento')
+ }
+
+
+//  Questão 6
+
+let estoque = 10
+
+ if(estoque<10){
+    console.log('Estoque baixo')
+ }
+ else if (estoque>10&&estoque>50){
+     console.log('Estoque médio')
+ }
+ else{
+    console.log('Estoque alto')
+ }
+
